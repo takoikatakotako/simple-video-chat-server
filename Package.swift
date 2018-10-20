efd96c60a1bd703ce34aaef8206125850938906d
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2018-10-20 16:13:28 +0900

    update packages

--- Package.swift
+++ Package.swift
@@ -9,9 +9,13 @@ let package = Package(
 
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        
+        // 🔵 Swift ORM (queries, models, relations, etc) built on MySQL
+        .package(url: "https://github.com/vapor/mysql.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Leaf", "Vapor"]),
+        .target(name: "App", dependencies: ["Leaf", "FluentMySQL", "MySQL", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
