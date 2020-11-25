4eaa3da76650f88ed921aa5e3f5b0f8a0746d42a
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2020-11-25 19:12:52 +0900

    templateファイルを使わないようにした。

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,7 +4,7 @@ import Leaf
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
     // register routes
     try routes(app)
